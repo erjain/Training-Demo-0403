b99e50eb12c125679fb8572a68b10f24214526d7
Tarun Jain <v-tjain@microsoft.com>
2022-03-08 09:49:01 +0530

    Add project files.

---
+++ Training Demo 0403/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Training_Demo_0403.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        ///[Controller]/[ActionName]/[Parameters]
+        // https://localhost:xxxx/Helloworld/Welcome
+        //Requests using GET should only be used to request data (they shouldn't include data).
+        //Query string- it will start with "?" after path
+        //Pass parameter: name=value combination separated by &
+        // https://localhost:[port]/HelloWOrld/Welcome?name=ABC&numTimes=4
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ Training Demo 0403/Data/Training_Demo_0403Context.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Training_Demo_0403.Models;
+
+namespace Training_Demo_0403.Data
+{
+    public class Training_Demo_0403Context : DbContext
+    {
+        public Training_Demo_0403Context (DbContextOptions<Training_Demo_0403Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Training_Demo_0403.Models.Movie> Movie { get; set; }
+    }
+}
